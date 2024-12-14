d95f5015d043a896f6df8c4704b0645009ba6cd6
João Lucas Sidney Rodrigues <43157844+JaoLucass@users.noreply.github.com>
2024-12-14 10:45:56 -0400

    Add files via upload

---
+++ symtable.cpp
@@ -0,0 +1,37 @@
+#include "symtable.h"
+
+// construtor para a primeira tabela
+SymTable::SymTable() : prev(nullptr)
+{
+}
+
+// construtor para novas tabelas
+SymTable::SymTable(SymTable * t) : prev(t)
+{		
+}
+
+// insere um símbolo na tabela
+bool SymTable::Insert(string s, Symbol symb) 
+{ 
+	auto result = table.insert({s, symb});
+	auto pos = result.first;
+	auto success = result.second;
+	return success;
+
+}
+
+// busca um símbolo na tabela atual 
+// se não encontrado, busca nas tabelas dos escopos envolventes
+Symbol * SymTable::Find(string s) 
+{
+	for (SymTable * st = this; st != nullptr; st = st->prev) 
+	{
+        auto found = st->table.find(s);
+        if (found != st->table.end()) 
+			return &found->second;
+    }
+
+    return nullptr;
+} 
+
+
